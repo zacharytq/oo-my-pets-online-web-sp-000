d1c1cf9cbf97aa557af964e88ee49391bd5cfcc0
chic-page-6502 <zacharytq@gmail.com>
2019-08-15 01:45:24 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,12 @@
 class Cat
   # code goes here
+  @@all = []
   attr_accessor :owner, :mood
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end
 
 end
